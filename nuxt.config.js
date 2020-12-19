e3724f6486e3505506fc518a634d4f9837f2a4ab
Thierry Marianne <thierry@marianne.io>
2020-12-19 23:57:07 +0100

    Revise app title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: "news-review",
+    title: "Daily Press Review",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
