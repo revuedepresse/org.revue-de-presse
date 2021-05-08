61821b2a5f6d5bc9bf8c9ad1e92f07c0a36a710f
Thierry Marianne <thierry@marianne.io>
2021-05-08 17:17:00 +0200

    Moved methods and computed properties to date mixin

--- components/calendar-month/calendar-month.vue
+++ components/calendar-month/calendar-month.vue
@@ -58,31 +58,6 @@ export default {
     }
   },
   methods: {
-    dateOfFirstVisibleDay() {
-      let firstVisibleDayCandidate = this.nameOfFirstDayOfMonth();
-      let dateCandidate = new Date(this.year, this.month, 1);
-
-      while  (firstVisibleDayCandidate !== 'Lun.') {
-        dateCandidate.setDate(dateCandidate.getDate() - 1);
-        firstVisibleDayCandidate = this.whichDayOfWeek(dateCandidate.getDay());
-      }
-
-      return dateCandidate;
-    },
-    dateOfLastVisibleDay() {
-      let lastVisibleDayCandidate = this.nameOfLastDayOfMonth();
-      let dateCandidate = new Date(this.year, this.month, this.totalDaysInMonth());
-
-      while  (lastVisibleDayCandidate !== 'Dim.') {
-        dateCandidate.setDate(dateCandidate.getDate() + 1);
-        lastVisibleDayCandidate = this.whichDayOfWeek(dateCandidate.getDay());
-      }
-
-      return dateCandidate;
-    },
-    daysOfWeek() {
-      return ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'];
-    },
     dayNumbers(rowNumber) {
       const shift = (rowNumber - 1) * 7
 
@@ -115,20 +90,6 @@ export default {
         + daysAfterSelectedMonth
       ) / 7;
     },
-    nameOfFirstDayOfMonth() {
-      return this.daysOfWeek()[new Date(this.year, this.month, 1).getDay()]
-    },
-    nameOfLastDayOfMonth() {
-      const lastDayNumberOfMonth = this.totalDaysInMonth();
-
-      return this.whichDayOfWeek([new Date(this.year, this.month, lastDayNumberOfMonth).getDay()]);
-    },
-    totalDaysInMonth() {
-      return new Date(this.year, this.month + 1, 0).getDate();
-    },
-    totalDaysInPreviousMonth() {
-      return new Date(this.year, this.month, 0).getDate();
-    },
     weekDayClasses(weekDay) {
       const defaultClass = 'calendar-month__day-number';
 
@@ -142,10 +103,7 @@ export default {
         [defaultClass]: true,
         'calendar-month__day-number--other-month': weekDay.getMonth() !== this.month,
       }
-    },
-    whichDayOfWeek(dayNumber) {
-      return this.daysOfWeek()[dayNumber];
-    },
+    }
   }
 }
 </script>
--- mixins/date.js
+++ mixins/date.js
@@ -1,7 +1,46 @@
 import Time from '../modules/time';
 
 export default {
+  computed: {
+    daysOfWeek() {
+      return ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'];
+    },
+  },
   methods: {
+    dateOfFirstVisibleDay() {
+      let firstVisibleDayCandidate = this.nameOfFirstDayOfMonth();
+      let dateCandidate = new Date(this.year, this.month, 1);
+
+      this.guardAgainstMissingMonthOrYear(firstVisibleDayCandidate);
+
+      while  (firstVisibleDayCandidate !== 'Lun.') {
+        dateCandidate.setDate(dateCandidate.getDate() - 1);
+        firstVisibleDayCandidate = this.whichDayOfWeek(dateCandidate.getDay());
+      }
+
+      return dateCandidate;
+    },
+    dateOfLastVisibleDay() {
+      let lastVisibleDayCandidate = this.nameOfLastDayOfMonth();
+      let dateCandidate = new Date(this.year, this.month, this.totalDaysInMonth());
+
+      this.guardAgainstMissingMonthOrYear(lastVisibleDayCandidate);
+
+      while  (lastVisibleDayCandidate !== 'Dim.') {
+        dateCandidate.setDate(dateCandidate.getDate() + 1);
+        lastVisibleDayCandidate = this.whichDayOfWeek(dateCandidate.getDay());
+      }
+
+      return dateCandidate;
+    },
+    formatDate(date) {
+      return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    },
+    guardAgainstMissingMonthOrYear(candidate) {
+      if (candidate === undefined) {
+        throw 'Definition of month and year properties, data, computed or props are to be defined'
+      }
+    },
     getCurrentDate() {
       return Time.today();
     },
@@ -10,6 +49,23 @@ export default {
     },
     getMaxDate() {
       return this.getCurrentDate();
+    },
+    nameOfFirstDayOfMonth() {
+      return this.daysOfWeek[new Date(this.year, this.month, 1).getDay()]
+    },
+    nameOfLastDayOfMonth() {
+      const lastDayNumberOfMonth = this.totalDaysInMonth();
+
+      return this.whichDayOfWeek([new Date(this.year, this.month, lastDayNumberOfMonth).getDay()]);
+    },
+    totalDaysInMonth() {
+      return new Date(this.year, this.month + 1, 0).getDate();
+    },
+    totalDaysInPreviousMonth() {
+      return new Date(this.year, this.month, 0).getDate();
+    },
+    whichDayOfWeek(dayNumber) {
+      return this.daysOfWeek[dayNumber];
     }
   }
 };
