e6312e7c6d0043e85bb2cd4e3d399a7c01494dfb
Th. Ma <thierrymarianne@users.noreply.github.com>
2023-01-12 21:47:24 +0100

    Adding legal notice page (#385)

--- components/highlight-list/highlight-list.vue
+++ components/highlight-list/highlight-list.vue
@@ -11,6 +11,7 @@
 
     <div
       class="highlight-list__content">
+
       <div
         v-if="isBaselineView"
         class="highlight-list__navigation">
@@ -45,11 +46,62 @@
         </ul>
 
         <LoadingSpinner
-          v-if="isLoadingSpinnerVisible"
+          v-if="isLoadingSpinnerVisible && isNotLegalNoticeRoute"
           :message="errorMessage"
           :show-error-message="showErrorMessage"
           :show-loading-spinner="showLoadingSpinner"
         />
+
+        <div
+          v-if="isLegalNoticeRoute"
+          class="highlight-list__legal-notice"
+        >
+          <h1>En vigueur au 07/10/14</h1>
+
+          <p>Conformément aux dispositions des Articles 6-III et<br />
+          19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l'économie numérique,
+          dite L.C.E.N., il est porté à la connaissance des utilisateurs et visiteurs, ci-après l'"Utilisateur",
+          du site https://revue-de-presse.org,<br />
+          ci-après le "Site", les présentes mentions légales.<br />
+
+          La connexion et la navigation sur le Site par l'Utilisateur implique acceptation intégrale et sans réserve des présentes mentions légales.
+          <br />
+
+          Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».</p>
+
+          <h2>ARTICLE 1 - L'EDITEUR</h2>
+
+          <p>L'édition et la direction de la publication du Site est assurée par Thierry Marianne, domicilié au<br />
+
+          5 boulevard Victor Hugo Poissy 78300, dont l'adresse e-mail est<br />
+
+          contact [chez]  revue [tiret] de [tiret] presse [point] org<br />
+
+          ci-après l'"Editeur".</p>
+
+          <h2>ARTICLE 2 - HEBERGEURS</h2>
+
+          <p>Les hébergeur du Site sont la société SCALEWAY, dont le siège social est situé au<br />
+          8 rue de la Ville l'Evêque, 75008 Paris, <br />
+          avec le numéro de téléphone : +33 (0)1 84 13 00 00<br />
+          ainsi que la société Netlify situé au<br />
+          44 Montgomery Street, Suite 300, San Francisco, California 94104, dont l'adresse de support est support@netlify.com</p>
+
+          <h2>ARTICLE 3 - ACCES AU SITE</h2>
+
+          <p>Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant d'une nécessité de maintenance.<br />
+
+          En cas de modification, interruption ou suspension du Site,<br />
+
+          l'Editeur ne saurait être tenu responsable.</p>
+
+          <h2>ARTICLE 4 - COLLECTE DES DONNEES</h2>
+
+          <p>Le Site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans la mesure où il ne collecte aucune donnée concernant les utilisateurs.<br />
+
+          Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site, sans autorisation de l'Editeur est prohibée
+          et pourra entraînée des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.</p>
+        </div>
 
       </div>
 
@@ -194,6 +246,10 @@ export default class HighlightList extends mixins(ApiMixin, DateMixin, StatusFor
   get highlights() {
     let highlights = this.items
 
+    if (this.isLegalNoticeRoute) {
+      return [{status: this.intro}]
+    }
+
     if (this.isBaselineView) {
       if (this.$device.isDesktop) {
         return [{status: this.intro}].concat(highlights)
@@ -278,6 +334,14 @@ export default class HighlightList extends mixins(ApiMixin, DateMixin, StatusFor
 
   get showLoadingSpinner(): boolean {
     return this.$fetchState.pending === true
+  }
+
+  get isNotLegalNoticeRoute() {
+    return ! this.isLegalNoticeRoute
+  }
+
+  get isLegalNoticeRoute() {
+    return this.$route.name === 'legal-notice'
   }
 
   get isLoadingSpinnerVisible() {
--- middleware/redirect.ts
+++ middleware/redirect.ts
@@ -2,7 +2,10 @@ import { Middleware } from '@nuxt/types'
 import Time from '../modules/time'
 
 const RedirectMiddleware: Middleware = ({ route, redirect }) => {
-  if (route.name !== 'daily-review') {
+  if (
+    route.name !== 'homepage' &&
+    route.name !== 'daily-review' &&
+    route.name !== 'legal-notice') {
     return redirect(`/${Time.today()}/`)
   }
 }
