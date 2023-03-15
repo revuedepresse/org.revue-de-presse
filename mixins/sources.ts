5352200a913e82c9cbc4c740a60a633d3e264ac9
Thierry Marianne <thierry@marianne.io>
2023-03-15 21:12:21 +0100

    Adding sources mixin

--- mixins/sources.ts
+++ mixins/sources.ts
@@ -1,6 +1,7 @@
 import type { Route } from 'vue-router'
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, mixins } from 'nuxt-property-decorator'
 import Sources from '../assets/sources.json'
+import ApiMixin from '../mixins/api'
 
 type Source = {
   name: string,
@@ -9,40 +10,46 @@ type Source = {
   url: string
 }
 
+export const isValidSourceRoute = ({ sortedSources, route }: { sortedSources: Source[], route: Route }): boolean => {
+  if (route.name === 'source' && route.params.twitterId) {
+    const twitterId: number = parseInt(route.params.twitterId, 10)
+    const filteredSource = sortedSources.find(s => s.twitterId === twitterId)
+
+    return typeof filteredSource !== 'undefined'
+  }
+
+  return false
+}
+
+export const sortSources = (): Source[] => {
+  if (typeof Sources === 'undefined') {
+    return []
+  }
+
+  const sources: Source[] = structuredClone(Sources)
+
+  return sources.sort((left: Source, right: Source) => {
+    if (left.username === right.username) {
+      return 0
+    }
+
+    if (left.username < right.username) {
+      return -1
+    }
+
+    return 1
+  })
+}
+
 export { Source }
 
 @Component
-export default class SourcesMixin extends Vue {
+export default class SourcesMixin extends mixins(ApiMixin) {
   get sortedSources (): Source[] {
-    if (typeof Sources === 'undefined') {
-      return []
-    }
-
-    const sources: Source[] = structuredClone(Sources)
-
-    return sources.sort((left: Source, right: Source) => {
-      if (left.username === right.username) {
-        return 0
-      }
-
-      if (left.username < right.username) {
-        return -1
-      }
-
-      return 1
-    })
+    return sortSources()
   }
 
-  isInvalidSourceRoute (route: Route): boolean {
-    const sortedSources = this.sortedSources
-
-    if (route.name === 'source' && route.params.twitterId) {
-      const twitterId: number = parseInt(this.$route.params.twitterId, 10)
-      const filteredSource = sortedSources.find(s => s.twitterId === twitterId)
-
-      return typeof filteredSource === 'undefined'
-    }
-
-    return true
+  isValidSourceRoute (route: Route): boolean {
+    return isValidSourceRoute({ sortedSources: this.sortedSources, route })
   }
 }
