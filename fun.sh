2f4aaf551bab1f45cfa126dc5ad882d2665e3885
Thierry Marianne <thierry@marianne.io>
2025-03-06 02:35:47 +0100

    Tried fixing build

--- fun.sh
+++ fun.sh
@@ -1,18 +1,10 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
 
 function move_pages() {
-    export IFS=$'\n'
-    export LC_ALL=C
+  	@export IFS=$'\n'
 
-    for page in $(find ./dist/* -type f | sort --reverse);
-    do
-      local path
-      path="$(echo "${page}" | sed -E 's#\.\/dist\/##g' | sed 's#\.html##g')"
-      \cat $page | \
-      sed -E 's#\{\{ date \}\}#'"${path}"'#g' > ./template.html
-      mv ./template.html $page
-    done
+  	for page in $(find ./dist/* -type f | sort --reverse);
+  	do
+  		\cat $page | sed -E 's#\{\{ date \}\}#'"$(echo "${page}" | sed -E 's#\.\/dist\/##g' | sed 's#\.html##g')"'#g' > ./template.html
+  		mv ./template.html $page
+  	done
 }
-
-set +Eeuo pipefail
