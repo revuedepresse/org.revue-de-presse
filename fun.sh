27bb8365ee5ac796b039639d8ee6b6c248b37b94
Thierry Marianne <thierry@marianne.io>
2025-03-06 02:45:22 +0100

    Tried fixing deployment

--- fun.sh
+++ fun.sh
@@ -1,10 +1,15 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
 
 function move_pages() {
-  	@export IFS=$'\n'
+    export IFS=$'\n'
 
-  	for page in $(find ./dist/* -type f | sort --reverse);
-  	do
-  		\cat $page | sed -E 's#\{\{ date \}\}#'"$(echo "${page}" | sed -E 's#\.\/dist\/##g' | sed 's#\.html##g')"'#g' > ./template.html
-  		mv ./template.html $page
-  	done
+    for page in $(find ./dist/* -type f | sort --reverse);
+    do
+      \cat $page | \
+      sed -E 's#\{\{ date \}\}#'"$(echo "${page}" | sed -E 's#\.\/dist\/##g' | sed 's#\.html##g')"'#g' > ./template.html
+      mv ./template.html $page
+    done
 }
+
+set +Eeuo pipefail
