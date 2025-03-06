c5d44268c393bc756973fb96063ee4d74f968a68
Th. Ma. <thierrymarianne@users.noreply.github.com>
2025-03-06 02:54:45 +0100

    Bluesky adapter (#843)
    Revised deps
    Tried fixing build
    Tried fixing deployment
    Export LC_ALL var
    
    ---------
    
    Co-authored-by: Thierry Marianne <refs/heads/main>

--- fun.sh
+++ fun.sh
@@ -1,10 +1,18 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
 
 function move_pages() {
-  	@export IFS=$'\n'
+    export IFS=$'\n'
+    export LC_ALL=C
 
-  	for page in $(find ./dist/* -type f | sort --reverse);
-  	do
-  		\cat $page | sed -E 's#\{\{ date \}\}#'"$(echo "${page}" | sed -E 's#\.\/dist\/##g' | sed 's#\.html##g')"'#g' > ./template.html
-  		mv ./template.html $page
-  	done
+    for page in $(find ./dist/* -type f | sort --reverse);
+    do
+      local path
+      path="$(echo "${page}" | sed -E 's#\.\/dist\/##g' | sed 's#\.html##g')"
+      \cat $page | \
+      sed -E 's#\{\{ date \}\}#'"${path}"'#g' > ./template.html
+      mv ./template.html $page
+    done
 }
+
+set +Eeuo pipefail
